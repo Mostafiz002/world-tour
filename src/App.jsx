b7435485e3bd06a8dcc52e0826a762f539f72086
Mostafiz002 <mo.mahin4@gmail.com>
2025-05-16 22:35:29 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Countries from "./components/countries/Countries";
+
+function App() {
+  return (
+    <>
+      <h2>Vite + React</h2>
+      <Countries></Countries>
+    </>
+  );
+}
+
+export default App;
