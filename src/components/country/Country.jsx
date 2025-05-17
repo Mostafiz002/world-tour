ce8080af3469859e11db86ca3f4748811f07528c
Mostafiz002 <mo.mahin4@gmail.com>
2025-05-17 09:40:07 +0600

    added toggle state based on click

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import "./Country.css";
 const Country = ({ country }) => {
-  const { name, flags } = country;
+  const { name, flags, population, area } = country;
+
+  const [visited, setVisited] = useState(false);
+
+  const handleVisit = () => {
+    setVisited(!visited);
+  };
+
   return (
     <div className="country">
       <h4>Name: {name.common}</h4>
       <img src={flags.png} alt="" />
+      <p>Population: {population}</p>
+      <p>Area: {area}</p>
+      <button onClick={handleVisit}>
+        {!visited ? "add to visit" : "remove from visit"}
+      </button>
+      <br />
+      {visited ? "Visited" : "not visited"}
     </div>
   );
 };
